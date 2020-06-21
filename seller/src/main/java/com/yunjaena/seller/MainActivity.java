3127a2322a6fa9a1deefa53bb74eb9beb6f1a520
Yunjae-Na <yunjaena@gmail.com>
2020-06-22 03:40:04 +0900

    Add application module
    Add application module

---
+++ seller/src/main/java/com/yunjaena/seller/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yunjaena.seller;
+
+import android.os.Bundle;
+
+import com.yunjaena.core.activity.ActivityBase;
+
+public class MainActivity extends ActivityBase {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.yunjaena.core.R.layout.activity_main);
+    }
+}
